68991abfd721316c32c2165d61d522f11ca7b01d
septiano-tumulo <s21910222@student.ac.id>
2021-03-16 16:06:55 +0800

    branch

---
+++ pertemuan_15/index.js
@@ -0,0 +1,10 @@
+//senin
+console.log("Baris pertama");
+//selasa
+console.log("Baris kedua");
+console.log("Baris ketiga");
+//rabu
+console.log("Baris keempat");
+console.log("Baris kelima");
+//kamis
+console.log("Baris keenam");
