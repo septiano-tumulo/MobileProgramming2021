c8079ca6cc40e6f2ee4c761de46dd66ac29a8c33
septiano-tumulo <70122209+septiano-tumulo@users.noreply.github.com>
2021-03-16 16:15:01 +0800

    tambah baris kedelapan

--- pertemuan_15/home.js
+++ pertemuan_15/home.js
@@ -1,10 +1,14 @@
 //rabu
 console.log("Baris pertama");
+
 //kamis
 console.log("Baris kedua");
-
 
 console.log("Baris ketiga");
 console.log("Baris keempat dari online");
 console.log("Baris kelima");
 console.log("Baris keenam");
+console.log("Baris ketujuh");
+
+//jumat
+console.log("baris kedelapan");
