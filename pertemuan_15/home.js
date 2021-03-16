0d8cb527490fd643ffdfbe48e3cde713c2d26038
septiano-tumulo <s21910222@student.ac.id>
2021-03-16 16:12:26 +0800

    home

---
+++ pertemuan_15/home.js
@@ -0,0 +1,10 @@
+//rabu
+console.log("Baris pertama");
+//kamis
+console.log("Baris kedua");
+
+
+console.log("Baris ketiga");
+console.log("Baris keempat dari online");
+console.log("Baris kelima");
+console.log("Baris keenam");
